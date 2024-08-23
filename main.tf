b6a3a48c6e18e219701c9848fc9b90566654e5f0
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-23 10:30:59 -0500

    Update main.tf
    updated subnets prefixes for new azureRM version

--- main.tf
+++ main.tf
@@ -194,16 +194,16 @@ resource "azurerm_virtual_network" "hub-vnet" {
   name                = "AZ-hub-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "10.0.0.0/24"
+    address_prefixes     = ["10.0.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.hubvnetNSG.id
   }
   subnet {
-    address_prefix     = "10.0.1.0/24"
+    address_prefixes     = ["10.0.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   subnet {
-    address_prefix     = "10.0.2.0/24"
+    address_prefixes     = ["10.0.2.0/24"]
     name                 = "AzureFirewallSubnet" 
   }
   timeouts {
@@ -222,12 +222,12 @@ resource "azurerm_virtual_network" "spoke-vnet" {
   name                = "AZ-spoke-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "10.250.0.0/24"
+    address_prefixes     = ["10.250.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "10.250.1.0/24"
+    address_prefixes     = ["10.250.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -283,7 +283,7 @@ resource "azurerm_route_table" "RT" {
   name                          = "all-to-fw"
   location                      = azurerm_resource_group.RG.location
   resource_group_name           = azurerm_resource_group.RG.name
-  disable_bgp_route_propagation = false
+  
 
   route {
     name           = "inet"
